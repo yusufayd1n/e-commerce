10993c61b61c114c1ef9fd148c138b37a965433d
yusufayd1n <yusufaydn24@gmail.com>
2024-08-10 18:59:46 +0300

    toolbar layout created and implemented to detail fragment

--- app/src/main/java/com/example/e_commerce/presentation/view/ProductDetailFragment.kt
+++ app/src/main/java/com/example/e_commerce/presentation/view/ProductDetailFragment.kt
@@ -1,15 +1,14 @@
 package com.example.e_commerce.presentation.view
 
-import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.e_commerce.R
-import com.example.e_commerce.databinding.FragmentHomeBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.e_commerce.databinding.FragmentProductDetailBinding
 import com.example.e_commerce.domain.model.Product
+import com.example.e_commerce.extension.visible
 
 class ProductDetailFragment : Fragment() {
 
@@ -18,7 +17,6 @@ class ProductDetailFragment : Fragment() {
     private var product: Product? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -38,9 +36,17 @@ class ProductDetailFragment : Fragment() {
     }
 
     private fun initView() {
-        product?.let { product ->
-            binding.product = product
+        binding.apply {
+            product?.let { argsProduct ->
+                productData = argsProduct
+                toolbar.toolbarTitle.text = argsProduct.name
+            }
+            toolbar.backButton.setOnClickListener {
+                findNavController().popBackStack()
+            }
+            toolbar.backButton.visible()
         }
+
     }
 
     override fun onDestroyView() {
